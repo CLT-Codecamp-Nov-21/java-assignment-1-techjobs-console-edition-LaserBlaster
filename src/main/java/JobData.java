80a323a88ddfb44c478a307b07a6dcb16264fca3
LaserBlaster <adamlongtutor@gmail.com>
2022-02-01 21:38:56 -0500

    changes made to jobdata java file

--- src/main/java/JobData.java
+++ src/main/java/JobData.java
@@ -8,6 +8,7 @@ import java.io.Reader;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.List;
 import java.util.Iterator;
 
