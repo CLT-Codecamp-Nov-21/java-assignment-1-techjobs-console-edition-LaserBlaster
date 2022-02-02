78187e4571364d690207de972faaa11b008517be
LaserBlaster <adamlongtutor@gmail.com>
2022-02-01 22:11:37 -0500

    chnages made to both files

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -122,7 +122,7 @@ public class TechJobs {
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
        
        if(someJobs.length == 0){
-            System.out.println("[No Results]")
+            System.out.println("[No Results]");
        }
        else{
        
